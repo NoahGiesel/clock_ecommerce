bd7f7c3b641547bf5ee7a1193cab6d15aa5733f2
Noah Giesel <noahgiesel@gmail.com>
2019-07-13 12:58:28 +0200

    clocks added js

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
+
+let immagineAttuale = 0 ; 
+const orologio1 = document.getElementById("orologio1");
+const orologio2 = document.getElementById("orologio2");
+const orologio3 = document.getElementById("orologio3");
+
+function buttonLeft() {  
+    if(immagineAttuale == 0 ) { 
+        orologio1.style.transform = "translate(100%, -50%) "
+        orologio2.style.transform = "translate(-50%, -50%) "
+        orologio3.style.transform = "translate(100%, -50%) "
+         immagineAttuale++; 
+        return;
+    }
+
+    if(immagineAttuale == 1 ) {
+        orologio1.style.transform = "translate(100%, -50%) "
+        orologio2.style.transform = "translate(100%, -50%) "
+        orologio3.style.transform = "translate(-50%, -50%) "
+         immagineAttuale++;
+        return;
+    }
+    if(immagineAttuale == 2 ) {
+        orologio1.style.transform = "translate(-50%, -50%) "
+        orologio2.style.transform = "translate(100%, -50%) "
+        orologio3.style.transform = "translate(100%, -50%) "
+         immagineAttuale = 0;
+        return;
+    }
  
+}
+function buttonRight() { 
+    console.log("cliccato right")
+}
