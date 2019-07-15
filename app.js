f52e1ca4573c47085b5fb2ee7b89329db9d185bc
Noah Giesel <noahgiesel@gmail.com>
2019-07-15 11:38:17 +0200

    ready for kart modal

--- app.js
+++ app.js
@@ -1,31 +1,61 @@
 let immagineAttuale=0;
+
+let clock = 0 ;  
+
+let contatoreNotifica = 0 ; 
+
+
 const orologio1=document.getElementById("orologio1");
 const orologio2=document.getElementById("orologio2");
 const orologio3=document.getElementById("orologio3");
 
 
+const titolo =document.getElementById("titolo");
+const materiale =document.getElementById("materiale");
+const descrizione =document.getElementById("descrizione");
+const prezzo =document.getElementById("prezzo");
+
+
+const notifica = document.getElementById("notifica") ; 
+
+
+
+
 // go forward img
 
 function buttonLeft(){
+
+    if(clock == 2) {
+        clock = 0; 
+        descrizioneFetch(clock);
+
+    }else {
+        clock++ ; 
+        descrizioneFetch(clock);
+
+    }
+  
+
+
     if(immagineAttuale == 0){
         orologio1.style.transform="translate(100%,-50%)"
         orologio2.style.transform="translate(-50%,-50%)"
         orologio3.style.transform="translate(100%,-50%)"
-        immagineAttuale++;
+         immagineAttuale++;
         return;
     }
     if(immagineAttuale == 1){
         orologio1.style.transform="translate(100%,-50%)"
         orologio2.style.transform="translate(100%,-50%)"
         orologio3.style.transform="translate(-50%,-50%)"
-        immagineAttuale++;
+         immagineAttuale++;
         return;
     }
     if(immagineAttuale == 2){
         orologio1.style.transform="translate(-50%,-50%)"
         orologio2.style.transform="translate(100%,-50%)"
         orologio3.style.transform="translate(100%,-50%)"
-        immagineAttuale=0;
+         immagineAttuale=0;
         return;
     }
 }
@@ -35,34 +65,76 @@ function buttonLeft(){
 
 
 function buttonRight(){
+
+ 
+    if(clock == 0) {
+        clock = 2; 
+        descrizioneFetch(clock);
+
+    }else {
+        clock-- ; 
+        descrizioneFetch(clock);
+
+    }
+
+
+
     if(immagineAttuale == 0){
         orologio1.style.transform="translate(100%,-50%)"
         orologio2.style.transform="translate(100%,-50%)"
         orologio3.style.transform="translate(-50%,-50%)"
-        immagineAttuale = 2 ;
+         immagineAttuale = 2 ;
         return;
     }
     if(immagineAttuale == 1){
         orologio1.style.transform="translate(-50%,-50%)"
         orologio2.style.transform="translate(100%,-50%)"
         orologio3.style.transform="translate(100%,-50%)"
-        immagineAttuale--;
+         immagineAttuale--;
         return;
     }
     if(immagineAttuale == 2){
         orologio1.style.transform="translate(100%,-50%)"
         orologio2.style.transform="translate(-50%,-50%)"
         orologio3.style.transform="translate(100%,-50%)"
-        immagineAttuale--;
+         immagineAttuale--;
         return;
     }   
  }
 
- //bottone che aggiunge alla cart per comprare 
+ //bottone che aggiunge alla cart per comprare  e notifica incrementa
 
- function buttonbuy(){
-    console.log("Buy bottone cliccato");
+ function buttonBuy(){
+    contatoreNotifica++;
+    notifica.innerHTML = contatoreNotifica;
 }
+
+
+
+
+
+
+
 
  // query descrizione.json
  
+
+ const descrizioneFetch = input => {
+
+    fetch("descrizione.json")
+    .then(response => response.json())
+    .then(data => {
+            
+            titolo.innerHTML = data[input].titolo;
+            materiale.innerHTML = data[input].materiale;
+            descrizione.innerHTML = data[input].descrizione;
+            prezzo.innerHTML = data[input].prezzo;
+        
+        })
+
+  
+ }
+
+
+ descrizioneFetch(clock);
+
